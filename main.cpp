8b8d46ac04c9c18c6d0140bf4a6ab9e456d33a1f
tebybee <tbybee@mail.csuchico.edu>
2018-04-17 11:44:57 -0700

    uses a function that uses pass-by-reference to arrange three integers into an order from least to greatest.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+void organize(int&, int&, int&);
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -8,7 +8,8 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
-
+  cout<<red<<green<<blue<< "\n";
+  organize(red,green, blue);
 
 
 
@@ -18,4 +19,41 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+void organize(int &red, int &green, int &blue)
+{
+    if((red > green) && (red > blue))
+    {
+        int temp = blue;
+        blue = red;
+        red = temp;
+        if(red > green)
+        {  
+            int temp = green;
+            green = red;
+            red = temp;
+        }
+    }
+    else if ((green > red) && (green > blue))
+    {   
+        int temp = blue;
+        blue = green; 
+        green = temp;
+        if(red > green)
+        {   
+           int temp = green;
+           green = red;
+           red = temp;
+        }
+    }
+    else if ((blue > red) && (blue > green))
+    {
+        if(red > green) 
+        {  
+            int temp = green;
+            green = red;
+            red = temp;
+        }
+    }
+    
 }
